54f0b12415c9b4766b77cbab1f10da6ec60778b6
ilavatlin <owtf086@gmail.com>
2026-02-23 00:25:18 +0700

    com 2

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello 2");
+        System.out.println("From task branch");
+        System.out.println("Hello World");
+    }
+}
